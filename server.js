4f3344f8a0694abeca5c42c60e69b0f7bcb3adfd
Oriane <31882053+o-louis@users.noreply.github.com>
2021-03-28 21:35:39 -0400

    feat(routes): houses

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ fastify.get("/", function (request, reply) {
   reply.send({ title: "Harry Potter API" });
 });
 
+fastify.get("/houses", function (request, reply) {
+  reply.send({ houses: ['gryffindor', 'ravenclaw', 'slytherin', 'hufflepuff'] });
+});
+
 fastify.listen(PORT, function (err, address) {
   if (err) throw err;
   console.log(`server listening on ${fastify.server.address().port}`);
