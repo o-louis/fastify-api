e748db4367047954bf397cbc84c26b9e5056f95f
Oriane <31882053+o-louis@users.noreply.github.com>
2021-04-06 21:54:16 -0400

    feat(route/houses): Create route and return houses

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ export default async (app) => {
   app.get("/", async (req, reply) => {
     return { hello: "world" };
   });
+  
+  app.get("/houses", async (req, reply) => {
+    return { houses: ['ravemclaw', 'gryffindor', 'slytherin', 'hufflepuff'] };
+  });
 };
